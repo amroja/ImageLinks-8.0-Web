39a5efdcb449eba6dcfa4179dd1d42e443f1f65c
Amro Ahmad <aahmad@optimizasolutions.com>
2025-11-03 17:17:00 +0300

    Your commit message

---
+++ src/app/components/tab-toolbar/tab-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-toolbar',
+  templateUrl: './tab-toolbar.component.html',
+  styleUrls: ['./tab-toolbar.component.css']
+})
+export class TabToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
